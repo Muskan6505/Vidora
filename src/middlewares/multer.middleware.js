7fa8c85397eec9b19c5ee6a4dbf2f54f8142cb9f
Muskan6505 <muskan1144m@gmail.com>
2025-04-09 12:56:53 +0530

    cloudinary and multer setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname) // can be same should be modified
+    }
+})
+
+export const upload = multer({ 
+    storage,
+})
